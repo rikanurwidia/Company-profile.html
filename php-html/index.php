f631a0ed6f1e6c093e34f637b8c906eef5c5e8d3
Rika Nurwidia <if23.rikanurwidia@mhs.ubpkarawang.ac.id>
2024-10-23 14:58:44 +0700

    Create index.php

---
+++ php-html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini Halaman Home</title>
+</head>
+<body>
+    <h1>Ini halaman login</h1>
+    <form action="./backend/login.php" method= "POST">
+        <input type="email" name="email" id="email" placeholder="email"/>
+        <input type="pasword" name="pasword" id="pasword" placeholder="pasword"/>
+        <button type="sumbit">login</button>
+    </form>
+
+</body>
+</html>
